4f0e63c3cabe21832cdbea795d725d4a9e5459d3
Pavan Boro <pavanboro14@gmail.com>
2018-06-20 16:41:24 +0530

    done with dashboard UI

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-// Get Homepage
-router.get('/', ensureAuthenticated, function(req, res){
-	res.render('index');
-});
-
 function ensureAuthenticated(req, res, next){
 	if(req.isAuthenticated()){
 		return next();
 	} else {
-		//req.flash('error_msg','You are not logged in');
 		res.redirect('/users/login');
 	}
 }
+
+// Get Homepage
+router.get('/',function(req, res){
+	res.render('home');
+});
+
+router.get('/dashboard', ensureAuthenticated, function(req, res){
+	res.render('dashboard');
+});
+
+router.get('/test', ensureAuthenticated, function(req, res){
+	res.render('test');
+});
+
 
 module.exports = router;
